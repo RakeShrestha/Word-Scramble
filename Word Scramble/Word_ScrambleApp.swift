44b2fbeb544221dd2e603aca595893ccd77ea1dd
RakeShrestha <orakeshstha7@gmail.com>
2024-12-30 20:52:10 +0545

    Initial Commit

---
+++ Word Scramble/Word_ScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_ScrambleApp.swift
+//  Word Scramble
+//
+//  Created by Rakesh Shrestha on 30/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct Word_ScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
